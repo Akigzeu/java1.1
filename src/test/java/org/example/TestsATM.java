8727870b0c53559a795b3a8a59f30ca313207f53
Akim <Akim.suslin@bk.ru>
2025-03-14 12:20:11 +0300

    рфрф

--- src/test/java/org/example/TestsATM.java
+++ src/test/java/org/example/TestsATM.java
@@ -22,7 +22,7 @@ public class TestsATM {
 
         Account account = new Account("12345", "1111");
         account.setId(1);
-        account.setBalance(Double.valueOf("100.00"));
+        account.setBalance(Double.valueOf("900.00"));
 
         when(mockAccountRepository.getAccount("12345", "1111")).thenReturn(java.util.Optional.of(account));
 
